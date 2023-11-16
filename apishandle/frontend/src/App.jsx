f473d9e14cdcdbd33f74659b754701b5953ba292
Rajan Gupta <rajangpt74@gmail.com>
2023-11-17 00:03:52 +0530

    complete api handle

--- apishandle/frontend/src/App.jsx
+++ apishandle/frontend/src/App.jsx
@@ -2,20 +2,64 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
-function App() {
-  const [producs, error, loading] = customRecatQuery('/api/products')
-  if (error) {
-    return <h1>Something went wrong!!</h1>
-  }
 
-  if (loading) {
-    return <h1>loadding..</h1>
-  }
+function App() {
+  // const [producs, error, loading] = customRecatQuery('/api/products')
+
+  const [producs, setProducts] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    const controller = new AbortController();
+    (async () => {
+      try {
+        setLoading(true)
+        setError(false)
+        const response = await axios.get('/api/products?search=' + search, {
+          signal: controller.signal
+        })
+        console.log(response.data);
+        setProducts(response.data)
+        setLoading(false)
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log('Request Canceled', error.message)
+          return
+        }
+        setError(true)
+        setLoading(false)
+      }
+    })() // IFFI
+
+    return () => {
+      controller.abort()
+    }
+  }, [search])
+
+
+  // if (error) {
+  //   return <h1>Something went wrong!!</h1>
+  // }
+
+  // if (loading) {
+  //   return <h1>loadding..</h1>
+  // }
 
   return (
     <>
 
       <h1>APIs Handle</h1>
+
+      <input type="text" placeholder='Search'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+
+      />
+      {loading && (<h1>Loading..</h1>)}
+      {error && <h1>Something went wrong!</h1>}
       <h2>Number of Product are: {producs.length}</h2>
     </>
   )
@@ -25,25 +69,6 @@ export default App
 
 
 const customRecatQuery = (urlPath) => {
-  const [producs, setProducts] = useState([])
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    (async () => {
-      try {
-        setLoading(true)
-        setError(false)
-        const response = await axios.get(urlPath)
-        console.log(response.data);
-        setProducts(response.data)
-        setLoading(false)
-      } catch (error) {
-        setError(true)
-        setLoading(false)
-      }
-    })() // IFFI
-  }, [])
   return [producs, loading, error]
-
 }
