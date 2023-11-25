ac4d8a06f057c3d336d8ff814aefe0cfc562c116
Rajan Gupta <rajangpt74@gmail.com>
2023-11-25 21:02:19 +0530

    some uptade

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -34,7 +34,7 @@ export class AuthService {
     try {
       return await this.account.createEmailSession(emai, password)
     } catch (error) {
-      throw.error;
+      throw(error);
     }
   }
 
